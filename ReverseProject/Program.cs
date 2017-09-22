18d40a065f0090ac79865907be1308b69737627a
Bill Altvater <baltvater@comcast.net>
2017-09-22 15:56:42 -0500

    refactor and simplify

--- ReverseProject/Program.cs
+++ ReverseProject/Program.cs
@@ -6,23 +6,17 @@ namespace ReverseProject
     {
         static void Main(string[] args)
         {
-            IItems<int> intArray = new IntItems();
-            intArray.Reverse(intArray.Items);
-            intArray.WriteItOut(intArray.ReversedItems);
-
-            IItems<string> strArray = new StringItems();
-            strArray.Reverse(strArray.Items);
-            strArray.WriteItOut(strArray.ReversedItems);
-
-            IItems<DateTime> dateArray = new DateItems();
-            dateArray.Reverse(dateArray.Items);
-            dateArray.WriteItOut(dateArray.ReversedItems);
-
-            IItems<Cat> catArray = new CatItems();
-            catArray.Reverse(catArray.Items);
-            catArray.WriteItOut(catArray.ReversedItems);
-
+            DoThatThing(new IntItems());
+            DoThatThing(new StringItems());
+            DoThatThing(new DateItems());
+            DoThatThing(new CatItems());
             Console.Read();
+        }
+
+        static void DoThatThing<T>(IItems<T> items)
+        {
+            items.Reverse(items.Items);
+            items.WriteItOut(items.ReversedItems);
         }
     }
 }
